ca70fa76a628dfe234feb682e35ec63c0c6c9455
limengdu <37475446+limengdu@users.noreply.github.com>
2023-06-14 09:48:16 +0800

    First push

---
+++ IP-address-location-method/IP-address-location-method.ino
@@ -0,0 +1,270 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <TFT_eSPI.h>
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+// Include library for Round Display
+#define USE_TFT_ESPI_LIBRARY
+#include "lv_xiao_round_screen.h"
+
+// Include the jpeg decoder library
+#include <TJpg_Decoder.h>
+
+// For network
+const char* ssid = "<YOUR_WIFI_SSID_HERE>";
+const char* password = "<YOUR_WIFI_PW_HERE>";
+
+// For ipstack
+const char* IPStack_key = "<YOUR_API_KEY_HERE>";
+//String ip_address = "113.87.163.2";
+
+// For google static maps
+const char * host = "maps.googleapis.com";
+const String defaultPath = "/maps/api/staticmap?center=";
+const String Googlemaps_key = "<YOUR_API_KEY_HERE>";
+const char * mapFile = "/map.jpg";
+int zoomLevel = 14;
+double latitude;
+double longitude;
+
+// For SD Card
+#define SD_CS D2
+#define FS_NO_GLOBALS
+
+
+// GlobalSign CA certificate valid until 28th January 2028
+static const char GlobalSignCA[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDdTCCAl2gAwIBAgILBAAAAAABFUtaw5QwDQYJKoZIhvcNAQEFBQAwVzELMAkG
+A1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExEDAOBgNVBAsTB1Jv
+b3QgQ0ExGzAZBgNVBAMTEkdsb2JhbFNpZ24gUm9vdCBDQTAeFw05ODA5MDExMjAw
+MDBaFw0yODAxMjgxMjAwMDBaMFcxCzAJBgNVBAYTAkJFMRkwFwYDVQQKExBHbG9i
+YWxTaWduIG52LXNhMRAwDgYDVQQLEwdSb290IENBMRswGQYDVQQDExJHbG9iYWxT
+aWduIFJvb3QgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDaDuaZ
+jc6j40+Kfvvxi4Mla+pIH/EqsLmVEQS98GPR4mdmzxzdzxtIK+6NiY6arymAZavp
+xy0Sy6scTHAHoT0KMM0VjU/43dSMUBUc71DuxC73/OlS8pF94G3VNTCOXkNz8kHp
+1Wrjsok6Vjk4bwY8iGlbKk3Fp1S4bInMm/k8yuX9ifUSPJJ4ltbcdG6TRGHRjcdG
+snUOhugZitVtbNV4FpWi6cgKOOvyJBNPc1STE4U6G7weNLWLBYy5d4ux2x8gkasJ
+U26Qzns3dLlwR5EiUWMWea6xrkEmCMgZK9FGqkjWZCrXgzT/LCrBbBlDSgeF59N8
+9iFo7+ryUp9/k5DPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMBAf8E
+BTADAQH/MB0GA1UdDgQWBBRge2YaRQ2XyolQL30EzTSo//z9SzANBgkqhkiG9w0B
+AQUFAAOCAQEA1nPnfE920I2/7LqivjTFKDK1fPxsnCwrvQmeU79rXqoRSLblCKOz
+yj1hTdNGCbM+w6DjY1Ub8rrvrTnhQ7k4o+YviiY776BQVvnGCv04zcQLcFGUl5gE
+38NflNUVyRRBnMRddWQVDf9VMOyGj/8N7yy5Y0b2qvzfvGn9LhJIZJrglfCm7ymP
+AbEVtQwdpf5pLGkkeB6zpxxxYu7KyJesF12KwvhHhm4qxFYxldBniYUr+WymXUad
+DKqC5JlR3XC321Y9YeRq4VzW9v493kHMB65jUr9TU/Qr6cf9tveCX4XSQRjbgbME
+HMUfpIBvFSDJ3gyICh3WZlXi/EjJKSZp4A==
+-----END CERTIFICATE-----
+)EOF";
+
+
+// This next function will be called during decoding of the jpeg file to
+// render each block to the TFT.  If you use a different TFT library
+// you will need to adapt this function to suit.
+bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap)
+{
+   // Stop further decoding as image is running off bottom of screen
+  if ( y >= tft.height() ) return 0;
+
+  // This function will clip the image block rendering automatically at the TFT boundaries
+  tft.pushImage(x, y, w, h, bitmap);
+
+  // Return 1 to decode next block
+  return 1;
+}
+
+
+// Stitching to form commands sent to Google Maps
+String getPath(){
+  String newPath = defaultPath;
+  newPath += latitude;
+  newPath += ",";
+  newPath += longitude;
+  newPath += "&zoom=";
+  newPath += String(zoomLevel);
+  newPath += "&size=240x240";
+  newPath += "&maptype=roadmap";
+  newPath += "&markers=size:tiny%7Ccolor:red%7C";
+  newPath += latitude;
+  newPath += ",";
+  newPath += longitude;
+  newPath += "&format=jpg-baseline";
+  newPath += "&key=";
+  newPath += Googlemaps_key;
+  Serial.println(newPath);
+  return newPath;
+}
+
+
+// Obtain the approximate coordinate position based on the current IP address of XIAO.
+bool getLocation(){
+  // Make HTTP request to IPStack API
+  HTTPClient http;
+  String url = "http://api.ipstack.com/" + String(ip_address) + "?access_key=" + String(IPStack_key);
+  Serial.println("Requesting URL: " + url);
+  http.begin(url);
+  int httpCode = http.GET();
+
+  // Parse JSON response
+  if (httpCode == 200) {
+    String payload = http.getString();
+    Serial.println("Response payload: " + payload);
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, payload);
+    String country_name = doc["country_name"].as<String>();
+    String region_name = doc["region_name"].as<String>();
+    String city = doc["city"].as<String>();
+    latitude = doc["latitude"].as<double>();
+    longitude = doc["longitude"].as<double>();
+    Serial.println("Country: " + country_name);
+    Serial.println("Region: " + region_name);
+    Serial.println("City: " + city);
+    Serial.println("Latitude: " + String(latitude));
+    Serial.println("Longitude: " + String(longitude));
+    http.end(); // Close connection
+    return true;
+  } else {
+    Serial.println("HTTP error code: " + String(httpCode));
+    http.end(); // Close connection
+    return false;
+  }
+}
+
+
+// Static images of coordinates from Google Cloud Services
+bool getStaticMapImage(const char *host, const char *path, String fileName){
+  int contentLength = 0;
+  int httpCode;
+
+  WiFiClientSecure client;
+
+  client.setCACert(GlobalSignCA);
+  client.connect(host, 443);
+
+  Serial.printf("Trying: %s:443...", host);
+  
+  if(!client.connected()){
+    client.stop();
+    Serial.printf("*** Can't connect. ***\n-------\n");
+    return false;
+  }
+
+  Serial.println("HTTPS Connected!");
+  client.print("GET ");
+  client.print(path);
+  client.print(" HTTP/1.0\r\nHost: ");
+  client.print(host);
+  client.print("\r\nUser-Agent: ESP32S3\r\n");
+  client.print("\r\n");
+
+  while(client.connected()){
+    String header = client.readStringUntil('\n');
+    if(header.startsWith(F("HTTP/1."))){
+      httpCode = header.substring(9, 12).toInt();
+      if(httpCode != 200){
+        client.stop();
+        return false;
+      }
+    }
+
+    if(header.startsWith(F("Content-Length: "))){
+      contentLength = header.substring(15).toInt();
+    }
+    
+    if(header == F("\r")){
+      break;
+    }
+    
+  }
+
+  if(!(contentLength > 0)){
+    client.stop();
+    return false;
+  }
+
+  fs::File f = SD.open(fileName, "w");
+  if(!f){
+    Serial.println(F("FILE OPEN FAILED"));
+    client.stop();
+    return false;
+  }
+
+  int remaining = contentLength;
+  int received;
+  uint8_t buff[512] = {0};
+
+  while(client.available() && remaining > 0){
+    received = client.readBytes(buff, ((remaining > sizeof(buff)) ? sizeof(buff) : remaining));
+    f.write(buff, received);
+
+    if(remaining > 0){
+      remaining -= received;
+    }
+    yield();
+  }
+
+  f.close();
+  client.stop();
+  Serial.println("DOWNLOAD END");
+  return (remaining == 0 ? true : false);
+}
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  while(!Serial);
+  delay(500);
+
+  // Initialise the TFT
+  tft.init();
+  tft.setRotation(2);
+  tft.fillScreen(TFT_BLACK);
+  tft.setSwapBytes(true); // We need to swap the colour bytes (endianess)
+
+  // Initialise SD before TFT
+  if (!SD.begin(SD_CS)) {
+    Serial.println(F("SD.begin failed!"));
+    return;
+  }
+  Serial.println("\r\nInitialisation done.");
+
+  Serial.print("Try to connect to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    vTaskDelay(500);
+    Serial.print(".");
+  }
+  Serial.println("Wi-Fi Connected!");
+  Serial.print("The IP address of the current network is: ");
+  
+  // Get local IP address
+  IPAddress ip = WiFi.localIP();
+  ip_address = ip.toString();
+  Serial.println(ip_address);
+
+  // The jpeg image can be scaled by a factor of 1, 2, 4, or 8
+  TJpgDec.setJpgScale(1);
+
+  // The decoder must be given the exact name of the rendering function above
+  TJpgDec.setCallback(tft_output);
+
+  if(WiFi.status() == WL_CONNECTED){
+    if(getLocation() && getStaticMapImage(host, getPath().c_str(), mapFile)){
+      TJpgDec.drawSdJpg(0, 0, mapFile);
+    }
+  }
+}
+
+
+
+void loop() {
+  
+
+}
